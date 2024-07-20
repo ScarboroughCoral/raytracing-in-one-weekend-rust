4d9a3edcadb532ce161f77de25694fa7ec745bda
Coral <corallee9797@gmail.com>
2024-07-20 06:22:10 +0000

    feat: impl Vec3 struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod vec3;
+
+
 fn main() {
     // Image
     let image_width = 256;
@@ -6,6 +9,7 @@ fn main() {
     println!("P3\n{} {}\n255", image_width, image_height);
 
     for j in 0..image_height {
+        dbg!("Scanlines remaining: ", image_height - j);
         for i in 0..image_width {
             let r = (i as f64)  / (image_width as f64 - 1.0);
             let g = (j as f64) / (image_height as f64 - 1.0);
@@ -17,4 +21,5 @@ fn main() {
 
         }
     }
+    dbg!("Done.           \n");
 }
---
+++ src/vec3.rs
@@ -0,0 +1,125 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub};
+
+struct Vec3(f64,f64,f64);
+
+type Point3 = Vec3;
+
+impl Vec3 {
+    fn new(e0: f64, e1: f64, e2: f64) -> Self {
+        return Vec3(e0,e1,e2);
+    }
+    fn x(&self) -> f64 {
+        self.0
+    }
+    fn y(&self) -> f64 {
+        self.1
+    }
+    fn z(&self) -> f64 {
+        self.2
+    }
+    fn dot_with(&self, rhs: &Self) -> f64 {
+        self.0 * rhs.0 + self.1 * rhs.1 + self.2 * rhs.2
+    }
+    fn cross_with(&self, rhs: &Self) -> Vec3 {
+        Vec3(
+            self.1 * rhs.2 - self.2 * rhs.1,
+            self.2 * rhs.0 - self.0 * rhs.2,
+            self.0 * rhs.1 - self.1 * rhs.0
+        )
+    }
+    fn unit_vector(&self) -> Vec3 {
+        self / self.length()
+    }
+    fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+    fn length_squared(&self) -> f64 {
+        self.0 * self.0 + self.1 * self.1 + self.2 * self.2
+    }
+}
+
+impl Default for Vec3 {
+    fn default() -> Self {
+        Self(Default::default(), Default::default(), Default::default())
+    }
+}
+
+
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Self::Output {
+        Vec3(-self.0,-self.1,-self.2)
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Vec3(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2)
+    }
+}
+
+impl Mul for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Vec3(self.0 * rhs.0, self.1 * rhs.1, self.2 * rhs.2)
+    }
+}
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec3(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3(self * rhs.0, self * rhs.1, self * rhs.2)
+    }
+}
+
+impl Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Vec3(self.0 / rhs, self.1 / rhs, self.2 / rhs)
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0;
+        self.1 += rhs.1;
+        self.2 += rhs.2;
+    }
+}
+
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.0 *= rhs.0;
+        self.1 *= rhs.1;
+        self.2 *= rhs.2;
+    }
+}
+
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, rhs: Self) {
+        self.0 /= rhs.0;
+        self.1 /= rhs.1;
+        self.2 /= rhs.2;
+    }
+}
